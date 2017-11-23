01a621bf1a020dc57fa82e57721d78d7f7263a8a
Harshit Kumar Singh <harshitks2203@gmail.com>
2017-11-23 15:34:34 +0530

    First commit

---
+++ src/Config/Firebase/db_config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyAx5JItbeN5ejeA4kMyRo5HMzyhqa8Jdlk",
+    authDomain: "react-flashcard-b35ec.firebaseapp.com",
+    databaseURL: "https://react-flashcard-b35ec.firebaseio.com",
+    projectId: "react-flashcard-b35ec",
+    storageBucket: "",
+    messagingSenderId: "939102064532"
+}
